43f9547a6db88fee49ed5e3e40760c62e1f86c22
Toomas Vahter <toomas.vahter@gmail.com>
2018-04-29 17:11:11 +0300

    Initial Commit

---
+++ CommonCryptoExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CommonCryptoExample
+//
+//  Created by Toomas Vahter on 29/04/2018.
+//  Copyright © 2018 Augmented Code. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
